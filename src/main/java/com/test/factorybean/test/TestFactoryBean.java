d54da76d167fbcd98ed82149ddf668e3949443d4
lmm <15060335532@139.com>
2019-03-05 08:51:32 +0800

    测试factorybean

---
+++ src/main/java/com/test/factorybean/test/TestFactoryBean.java
@@ -0,0 +1,24 @@
+package com.test.factorybean.test;
+
+import com.test.customtag.pojo.Student;
+import com.test.customtag.pojo.User;
+import com.test.factorybean.pojo.Car;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 版权声明：Copyright(c) 2019
+ *
+ * @program: ssmweb
+ * @Author minmin.liu
+ * @Date 2019-03-02 16:50
+ * @Version 1.0
+ * @Description 测试factorybean
+ */
+public class TestFactoryBean {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("factorybean/car-factorybean.xml");
+        Car car = (Car) applicationContext.getBean("car");
+        System.out.println(car.getBrand() + ";" + car.getMaxSpeed() + ";" + car.getPrice());
+    }
+}
