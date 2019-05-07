d08e18792a7c0aa82303a31d70961511beeef0c2
lmm <15060335532@139.com>
2019-05-07 09:39:37 +0800

    effective java

---
+++ src/main/java/com/test/effectivejava/six/Operation.java
@@ -0,0 +1,34 @@
+package com.test.effectivejava.six;
+
+/**
+ * @Auther: linklmm
+ * @Date: 2019/5/7 09:28* @Description 特定于常量的方法实现
+ */
+public enum Operation {
+    PLUS {
+        @Override
+        public double apply(double x, double y) {
+            return x + y;
+        }
+    },
+    MINUS {
+        @Override
+        public double apply(double x, double y) {
+            return x - y;
+        }
+    },
+    TIMES {
+        @Override
+        public double apply(double x, double y) {
+            return x * y;
+        }
+    },
+    DIVIDE {
+        @Override
+        public double apply(double x, double y) {
+            return x / y;
+        }
+    };
+
+    public abstract double apply(double x, double y);
+}
