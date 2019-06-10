9982dfce746b709a5c0e48f7098f0252139026a8
lmm <15060335532@139.com>
2019-06-10 11:45:51 +0800

    effective java

---
+++ src/main/java/com/test/effectivejava/eight/CollectionClassifier.java
@@ -0,0 +1,38 @@
+package com.test.effectivejava.eight;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @Auther: linklmm
+ * @Date: 2019/6/10 08:20* @Description 它试图根据一个集合是set、list，还是其他的集合类型，来对它进行分类
+ * 要调用哪个重载方法是在编译时就已经做出了决定
+ */
+public class CollectionClassifier {
+    public static String classify(Set<?> s) {
+        return "Set";
+    }
+
+    public static String classify(List<?> list) {
+        return "List";
+    }
+
+    public static String classify(Collection<?> c) {
+        return "Unknown Collection";
+    }
+
+    public static void main(String[] args) {
+        Collection<?>[] collections = {
+                new HashSet<String>(),
+                new ArrayList<BigInteger>(),
+                new HashMap<String, String>().values()};
+        for (Collection<?> c : collections){
+            System.out.println(classify(c));
+        }
+    }
+}
---
+++ src/main/java/com/test/effectivejava/eight/SetList.java
@@ -0,0 +1,33 @@
+package com.test.effectivejava.eight;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @Auther: linklmm
+ * @Date: 2019/6/10 08:47
+ * @Description list的remove的重载方法的实现和set的重载方法不一样
+ * List每remove掉一个元素以后，后面的元素都会向前移动，此时如果执行i=i+1，则刚刚移过来的元素没有被读取。
+ */
+public class SetList {
+    public static void main(String[] args) {
+        Set<Integer> set = new TreeSet<>();
+        List<Integer> list = new ArrayList<>();
+        for (int i = -3; i < 3; i++) {
+            set.add(i);
+            list.add(i);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            set.remove(i);
+//            list.remove(i);
+//            list.remove((Integer) i);
+            if (list.get(i).equals(-3)){
+                list.remove(i);
+            }
+        }
+        System.out.println(set + " " + list);
+    }
+}
