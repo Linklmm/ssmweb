8aee73d3039e1a69531233ad5deb3f091a2bd85c
playboy <15060335532@139.com>
2021-06-22 14:14:38 +0800

    3-sum问题

--- src/main/java/com/test/algorithm/book/one/four/TreeSum.java
+++ src/main/java/com/test/algorithm/book/one/four/TreeSum.java
@@ -1,17 +1,24 @@
 package com.test.algorithm.book.one.four;
 
+import java.util.Collections;
+import java.util.Map;
+
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Maps;
+
 /**
  * 3-sum
  * 求多少组满足三个整数相加为0的数组
  */
 public class TreeSum {
-    public static int count(int[] a){
+
+    public static int count(int[] a) {
         int N = a.length;
         int count = 0;
-        for (int i = 0;i<N;i++){
-            for (int j = i+1;j<N;j++){
-                for (int k = j+1;k<N;j++){
-                    if (a[i]+a[j]+a[k]==0){
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; j++) {
+                    if (a[i] + a[j] + a[k] == 0) {
                         count++;
                     }
                 }
@@ -21,7 +28,9 @@ public class TreeSum {
     }
 
     public static void main(String[] args) {
-
+        System.out.println(Collections.singletonMap("lll", "test").toString());
+        System.out.println(JSON.toJSONString(Collections.singletonMap("mmmm","nnnn")));
+        Map<String,String> map = Maps.newHashMap();
+        System.out.println(map.get(null));
     }
-
 }
--- src/main/java/com/test/test/Test2.java
+++ src/main/java/com/test/test/Test2.java
@@ -7,11 +7,22 @@ import com.test.test.pojo.BaseResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class Test2 {
+
     @Test
     public void testArray() {
         Base base1 = new Base(11, 11, false);
@@ -31,4 +42,41 @@ public class Test2 {
         baseResponse.setBases(null == bases || bases.isEmpty() ? Collections.emptyList() : bases);
         log.error("baseResponse:{}", JSON.toJSONString(baseResponse));
     }
+
+    @Test
+    public void testDistinct() {
+        List<Long> arry = Lists.newArrayList(1L, 2L, 3L, 3L, 2L, 1L, 4L, 5L, 1L, 2L);
+        arry = arry.stream().distinct().collect(Collectors.toList());
+        log.error("array:{}", JSON.toJSONString(arry));
+    }
+
+    @Test
+    public void testDateTime() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        // int day = localDateTime.getDayOfMonth();
+        localDateTime = localDateTime.with(TemporalAdjusters.firstDayOfMonth());
+        localDateTime = localDateTime.withHour(0);
+        localDateTime = localDateTime.withMinute(0);
+        localDateTime = localDateTime.withSecond(0);
+        localDateTime = localDateTime.withNano(0);
+
+        LocalDateTime endTime = localDateTime.with(TemporalAdjusters.lastDayOfMonth());
+        endTime = endTime.withHour(23);
+        endTime = endTime.withMinute(59);
+        endTime = endTime.withSecond(59);
+
+        System.out.println(localDateTime);
+        System.out.println(endTime);
+
+        System.out.println(localDateTime.toInstant(OffsetDateTime.now()
+            .getOffset()));
+    }
+    @Test
+    public void testLocalDateTime(){
+        LocalDateTime localDateTime = LocalDateTime.of(2021,6,11,14,30,0);
+        LocalDateTime localDateTime1 = LocalDateTime.of(2021,6,11,14,0,0);
+        System.out.println(localDateTime.toInstant(OffsetDateTime.now().getOffset()));
+        System.out.println(localDateTime1.toInstant(OffsetDateTime.now().getOffset()));
+    }
+
 }
