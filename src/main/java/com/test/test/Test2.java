0643ebf2da9984bda5e5a1b2b1b6b242e54d50ec
minmin.liu <minmin.liu@linkedcare.cn>
2022-06-20 15:18:04 +0800

    feat：时间的一些操作

--- src/main/java/com/test/test/Test2.java
+++ src/main/java/com/test/test/Test2.java
@@ -7,11 +7,13 @@ import com.test.test.pojo.Base;
 import com.test.test.pojo.BaseResponse;
 
 import java.math.BigDecimal;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Period;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -29,37 +31,40 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class Test2 {
 
-  @Test
-  public void testArray() {
-    Base base1 = new Base(11, 11, false);
-    Base base2 = new Base(12, 12, true);
-    List<Base> bases = Lists.newArrayList(base1, base2);
-    Base arrayBase = bases.stream().findFirst().get();
-    log.error("array:{}", JSON.toJSONString(bases));
-    log.error("findAndy:{}", JSON.toJSONString(bases.stream().findAny()));
-    arrayBase.setI(111);
-    log.error("array:{}", JSON.toJSONString(bases));
-  }
+    @Test
+    public void testArray() {
+        Base base1 = new Base(11, 11, false);
+        Base base2 = new Base(12, 12, true);
+        List<Base> bases = Lists.newArrayList(base1, base2);
+        Base arrayBase = bases.stream().findFirst().get();
+        log.error("array:{}", JSON.toJSONString(bases));
+        log.error("findAndy:{}", JSON.toJSONString(bases.stream().findAny()));
+        arrayBase.setI(111);
+        log.error("array:{}", JSON.toJSONString(bases));
+    }
 
-  @Test
-  public void testJson() {
-    BaseResponse baseResponse = new BaseResponse();
-    List<Base> bases = null;
-    baseResponse.setBases(null == bases || bases.isEmpty() ? Collections.emptyList() : bases);
-    log.error("baseResponse:{}", JSON.toJSONString(baseResponse));
-  }
+    @Test
+    public void testJson() {
+        BaseResponse baseResponse = new BaseResponse();
+        List<Base> bases = null;
+        baseResponse.setBases(null == bases || bases.isEmpty() ? Collections.emptyList() : bases);
+        log.error("baseResponse:{}", JSON.toJSONString(baseResponse));
+    }
 
-  @Test
-    public void testLocalDate() {
+    @Test
+    public void testLocalDate() throws InterruptedException {
         BigDecimal d = new BigDecimal("1.1111");
         log.error("d:{}", d);
         LocalDate localDate = LocalDate.now();
@@ -77,7 +82,7 @@ public class Test2 {
         log.error("last:{}", localDate.with(TemporalAdjusters.lastDayOfMonth()));
         log.error("month:{}", localDate.getMonthValue());
         LocalDate now = LocalDate.now();
-        LocalDate date = LocalDate.of(2022, 6, 18);
+        LocalDate date = LocalDate.of(2021, 6, 1);
         log.error("months:{}", Period.between(date, now).getMonths());
         log.error("months2:{}", now.getMonthValue() - date.getMonthValue());
         log.error("years:{}", Period.between(date, now).getYears());
@@ -87,40 +92,44 @@ public class Test2 {
         log.error("int:{}", localDate.getYear() * 10000 + localDate.getMonthValue() * 100 + localDate.getDayOfMonth());
         Instant nowI = Instant.now();
         log.error("nowI:{}", LocalDateTime.ofInstant(nowI, ZoneId.systemDefault()));
+        log.error("nowILocalDate:{}",nowI.atZone(ZoneId.systemDefault()).toLocalDate());
         log.error("plusSeconds:{}", LocalDateTime.ofInstant(nowI.plusSeconds(60), ZoneId.systemDefault()));
         log.error("instant:{}", Instant.now().toEpochMilli());
 
-        LocalDateTime localDateTime = LocalDateTime.of(20022, 5, 15, 20, 20);
-        LocalDateTime localDateTime2 = LocalDateTime.of(20022, 5, 15, 20, 20);
+        LocalDateTime localDateTime = LocalDateTime.of(20022, 1, 1, 20, 20);
+        LocalDateTime localDateTime2 = LocalDateTime.of(20022, 5, 19, 20, 20);
+        log.error("days:{}", ChronoUnit.DAYS.between(now, date));
         log.error("{}", !localDateTime.toLocalDate().equals(localDateTime2.toLocalDate()));
 
         log.error("between:{}", Period.between(date, now).getDays());
+        Thread.sleep(300);
+        log.error("duration:{}", Duration.between(nowI,Instant.now()).toMillis());
     }
 
     @Test
-  public void testDistinct() {
-    List<Long> arry = Lists.newArrayList(1L, 2L, 3L, 3L, 2L, 1L, 4L, 5L, 1L, 2L);
-    arry = arry.stream().distinct().collect(Collectors.toList());
-    log.error("array:{}", JSON.toJSONString(arry));
-  }
+    public void testDistinct() {
+        List<Long> arry = Lists.newArrayList(1L, 2L, 3L, 3L, 2L, 1L, 4L, 5L, 1L, 2L);
+        arry = arry.stream().distinct().collect(Collectors.toList());
+        log.error("array:{}", JSON.toJSONString(arry));
+    }
 
-  @Test
-  public void testDateTime() {
-    LocalDateTime localDateTime = LocalDateTime.now();
-    // int day = localDateTime.getDayOfMonth();
-    localDateTime = localDateTime.with(TemporalAdjusters.firstDayOfMonth());
-    localDateTime = localDateTime.withHour(0);
-    localDateTime = localDateTime.withMinute(0);
-    localDateTime = localDateTime.withSecond(0);
-    localDateTime = localDateTime.withNano(0);
+    @Test
+    public void testDateTime() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        // int day = localDateTime.getDayOfMonth();
+        localDateTime = localDateTime.with(TemporalAdjusters.firstDayOfMonth());
+        localDateTime = localDateTime.withHour(0);
+        localDateTime = localDateTime.withMinute(0);
+        localDateTime = localDateTime.withSecond(0);
+        localDateTime = localDateTime.withNano(0);
 
-    LocalDateTime endTime = localDateTime.with(TemporalAdjusters.lastDayOfMonth());
-    endTime = endTime.withHour(23);
-    endTime = endTime.withMinute(59);
-    endTime = endTime.withSecond(59);
+        LocalDateTime endTime = localDateTime.with(TemporalAdjusters.lastDayOfMonth());
+        endTime = endTime.withHour(23);
+        endTime = endTime.withMinute(59);
+        endTime = endTime.withSecond(59);
 
-    System.out.println(localDateTime);
-    System.out.println(endTime);
+        System.out.println(localDateTime);
+        System.out.println(endTime);
 
         System.out.println(localDateTime.toInstant(OffsetDateTime.now()
                 .getOffset()));
@@ -134,15 +143,15 @@ public class Test2 {
         System.out.println(localDateTime1.toInstant(OffsetDateTime.now().getOffset()));
     }
 
-  @Test
-  public void testArrays() {
-    //这里只是得到一个list视图，所以无法进行删除等操作
-    List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
-    System.out.println(JSON.toJSONString(list));
-    //不支持删除操作
-    list.remove(1);
-    System.out.println(JSON.toJSONString(list));
-  }
+    @Test
+    public void testArrays() {
+        //这里只是得到一个list视图，所以无法进行删除等操作
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        System.out.println(JSON.toJSONString(list));
+        //不支持删除操作
+        list.remove(1);
+        System.out.println(JSON.toJSONString(list));
+    }
 
     @Test
     public void testBigDecimal() {
@@ -163,6 +172,12 @@ public class Test2 {
         }
         listMap.forEach((key, value) -> value.remove(1));
         log.error("listMap:{}", listMap);
+
+        Map<Integer, Integer> linkedMap = new LinkedHashMap<>();
+        for (int i = 0; i < 10; i++) {
+            linkedMap.put(i, i);
+        }
+        log.error("linkedMap:{}",linkedMap);
 
     }
 
@@ -219,25 +234,44 @@ public class Test2 {
         log.error("integers:{}", JSON.toJSONString(integers));
     }
 
-  @Test
-  public void testObjectHash() {
-    Base base = new Base(1000, 1000, false);
-    Base base2 = new Base(1000, 1000, false);
-    Base base3 = new Base(1000, 1000, true);
-    log.error("base hash:{},base2 hash:{},base3 hash:{}", base.hashCode(), base2.hashCode(),
-        base3.hashCode());
-    Set<Base> bases = new HashSet<>();
-    bases.add(base);
-    bases.add(base2);
-    bases.add(base3);
+    @Test
+    public void testObjectHash() {
+        Base base = new Base(1000, 1000, false);
+        Base base2 = new Base(1000, 1000, false);
+        Base base3 = new Base(1000, 1000, true);
+        log.error("base hash:{},base2 hash:{},base3 hash:{}", base.hashCode(), base2.hashCode(),
+                base3.hashCode());
+        Set<Base> bases = new HashSet<>();
+        bases.add(base);
+        bases.add(base2);
+        bases.add(base3);
 
-    log.error("contains:{},size:{}", bases.contains(new Base(1000, 1000, false)), bases.size());
+        log.error("contains:{},size:{}", bases.contains(new Base(1000, 1000, false)), bases.size());
 
-  }
+    }
 
-  @Test
-  public void testHashCode() {
-    log.error("hashCode:{}", "abc".hashCode());
-  }
+    @Test
+    public void testHashCode() {
+        log.error("hashCode:{}", "abc".hashCode());
+    }
+
+    @Test
+    public void testException(){
+        Test2 test2 = new Test2();
+        try {
+            test2.testExce();
+        } catch (IllegalArgumentException e) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException interruptedException) {
+            }
+            test2.testExce();
+        }
+    }
+
+    public void testExce(){
+        log.error("重新执行");
+        throw new IllegalArgumentException();
+    }
 
 }
